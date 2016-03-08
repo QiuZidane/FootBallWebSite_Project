f2d7d7439914c60f71918beb6b121f3007abdfd9
kfzx-qiusd <qiusd@sdc.icbc.com.cn>
2016-03-08 22:50:46 +0800

    完成算法和大项同步变化

--- WebContent/WebSiteSrc/js/myabbs.js
+++ WebContent/WebSiteSrc/js/myabbs.js
@@ -68,12 +68,12 @@ var defen_abbs3 = $('#defen_abbs3').slider()
 //球员能力对象,记录所有的能力   
 var ability = {
 	//大项:
-	totalabi: 60, //总实力
-	body_abi: 60, //身体能力
-	tech_abi: 60, //技术能力
-	spec_abi: 60, //特殊能力
-	attack_abi: 60, //进攻能力
-	defence_abi: 60, //防守能力
+	totalabi: 50, //总实力
+	body_abi: 50, //身体能力
+	tech_abi: 50, //技术能力
+	spec_abi: 50, //特殊能力
+	attack_abi: 50, //进攻能力
+	defence_abi: 50, //防守能力
 	//细项:
 	speed: bodeabbs1.getValue(), //速度
 	strength: bodeabbs2.getValue(), //强壮
@@ -95,10 +95,17 @@ var ability = {
 
 }
 
+// 获取大项的progressBar
+var body_progress = document.getElementById("body_abi_pg");
+var tech_progress = document.getElementById("tech_abi_pg");
+var spec_progress = document.getElementById("spec_abi_pg");
+var attack_progress = document.getElementById("attack_abi_pg");
+var defence_progress = document.getElementById("defence_abi_pg");
+
 //  1、获取球员每个小项的能力
 //	2、计算大项能力和总实力
 function GetandCalPlayerAbilities() {
-	//需要重新获取小项的能力
+	//*****获取小项的能力*****
 	ability.speed = bodeabbs1.getValue(); //速度
 	ability.strength = bodeabbs2.getValue(); //强壮
 	ability.stamina = bodeabbs3.getValue(); //体能
@@ -115,16 +122,58 @@ function GetandCalPlayerAbilities() {
 	ability.taking = defen_abbs1.getValue(); //抢断
 	ability.marking = defen_abbs2.getValue(); //盯人
 	ability.positioning = defen_abbs3.getValue(); //防守站位
-	
-	//计算大项能力
-	
 
-//	for (var key in ability) {
-//		console.log(key + "=" + ability[key]); //speed = xx
-//	}
+	//*****计算大项能力*****
+	//身体属性，共四项
+	var speed_w = 0.35; //速度权重值
+	var strength_w = 0.2; //强壮权重值
+	var stamina_w = 0.35; //体能权重值
+	var health_w = 0.15; //受伤抗性	
+	ability.body_abi = ability.speed * speed_w + ability.strength * strength_w + ability.stamina * stamina_w + ability.health * health_w;
+	ability.body_abi.toFixed(2);
 
-	//	console.log(ability.speed);
-	//  console.log(bodeabbs1.getValue());
+	//技术属性，共四项
+	var passing_w = 0.3; //传球权重值
+	var touching_w = 0.3; //停球权重值
+	var dribbling_w = 0.3; //盘带权重值
+	var heading_w = 0.1; //头球权重值
+	ability.tech_abi = ability.passing * passing_w + ability.touching * touching_w + ability.dribbling * dribbling_w + ability.heading * heading_w;
+	ability.tech_abi.toFixed(2);
+
+	//特殊属性，共两项
+	var minding_w = 0.4; //意志力权重值
+	var rating_w = 0.6; //出勤率权重值
+	ability.spec_abi = ability.minding * minding_w + ability.rating * rating_w;
+	ability.spec_abi.toFixed(2);
+
+	//进攻属性，共三项
+	var shoot_w = 0.3; //射门
+	var offtheball_w = 0.45; //跑位
+	var creativity_w = 0.25; //创造力
+	ability.attack_abi = ability.shoot * shoot_w + ability.offtheball * offtheball_w + ability.creativity * creativity_w;
+	ability.attack_abi.toFixed(2);
+
+	//防守属性，共三项
+	var taking_w = 0.35; //抢断
+	var marking_w = 0.25; //盯人
+	var positioning_w = 0.4; //防守站位
+	ability.defence_abi = ability.taking * taking_w + ability.marking * marking_w + ability.positioning * positioning_w; //防守能力
+	ability.defence_abi.toFixed(2);
+
+	// 设置大项的属性条	
+	body_progress.setAttribute('style', 'width: ' + ability.body_abi + '%');
+	tech_progress.setAttribute('style', 'width: ' + ability.tech_abi + '%');
+	spec_progress.setAttribute('style', 'width: ' + ability.spec_abi + '%');
+	attack_progress.setAttribute('style', 'width: ' + ability.attack_abi + '%');
+	defence_progress.setAttribute('style', 'width: ' + ability.defence_abi + '%');
+
+
+	//	console.clear();	
+	//	for (var key in ability) {
+	//		if (ability[key] != 50) {
+	//			console.log(key + "=" + ability[key]); //speed = xx
+	//		}
+	//	}
 
 };
 
@@ -141,7 +190,8 @@ function submitABI() {
 }
 
 //改变slider-selection的颜色
-$('#tech_abbs1 .slider-selection').css('background', 'rgb(120, 142, 207)');
+//$('#tech_abbs1 .slider-selection').css('background', 'rgb(120, 142, 207)');
+//$('.slider-selection').css('background', 'rgb(120, 142, 207)');
 
 
 /* 
