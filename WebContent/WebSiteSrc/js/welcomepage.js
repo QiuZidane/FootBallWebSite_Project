0886021bd48ecec0d2f8b8c8888183bd0a3a2761
kfzx-qiusd <qiusd@sdc.icbc.com.cn>
2016-03-27 17:02:23 +0800

    欢迎界面完工，待整合flowc接口

--- WebContent/WebSiteSrc/js/welcomepage.js
+++ WebContent/WebSiteSrc/js/welcomepage.js
@@ -18,7 +18,7 @@ function countDownTime() {
 	$('#day').html(day);
 	$('#hour').html(hour);
 	$('#minute').html(minute);
-//	$('#second').html(second);
+	//	$('#second').html(second);
 
 	function checkTime(time) {
 		if (time < 10) {
@@ -28,7 +28,6 @@ function countDownTime() {
 	}
 }
 setInterval(countDownTime, 1000);
-
 
 // 每次进入页面访问数据库，获得player总人数，刷新参加人数统计
 // ajax 获取所有球员的属性，进入页面时获取一次，生成球员列表 
@@ -40,7 +39,7 @@ function GetPlayerCount() {
 		url: clubserver.URL + "A3GetPlayerData",
 		//提交的数据
 		data: {
-			joinflag : 0
+			joinflag: 0
 		},
 		//返回数据的格式
 		datatype: "html", //"xml", "html", "script", "json", "jsonp", "text".
@@ -56,10 +55,9 @@ function GetPlayerCount() {
 		complete: function(XMLHttpRequest, textStatus) {
 			//alert(XMLHttpRequest.responseText); //XMLHttpRequest.responseText是返回的信息，用这个来放JSON数据
 			try {
-				var jsonObject = JSON.parse(XMLHttpRequest.responseText);				
-				var playercount = jsonObject['totalnum'];				
+				var jsonObject = JSON.parse(XMLHttpRequest.responseText);
+				var playercount = jsonObject['totalnum'];
 				$('#CDjoins').html(playercount);
-
 
 			} catch (e) {
 				console.log("error=" + e.message);
@@ -75,15 +73,66 @@ function GetPlayerCount() {
 }
 
 // 按钮事件，链接到注册判断页面
-$('#joinBtn').click(function(){
+$('#joinBtn').click(function() {
 	document.location.href = '../pages/loginNavigation.html';
 });
 
 // 背景图片变化功能
 var teambg = document.getElementById('teambg');
-function teamBgChange(){
-	
+var bgindex = 0;
+
+// 透明度渐隐
+function teamBgChange() {
+
+	var bgopacity = 1;
+	opacityReduce();
+
+	function opacityReduce() {
+		if (bgopacity > 0) {
+			bgopacity -= 0.05;
+			$('#teambg').css({
+				'opacity': bgopacity
+			});
+			setTimeout(opacityReduce, 50);
+		} else {
+		}
+	}
 }
 
+function teamBgShow() {
+	// 先把透明度置为0
+	var bgopacity = 0;
+	$('#teambg').css({
+		'opacity': bgopacity
+	});
+	
+	// 按顺序读取球队图片
+	if (bgindex < 9) {
+		bgindex++;
+	} else {
+		bgindex = 1;
+	}
+	var bgname = "t" + bgindex + ".png";
+	
+	$('#teambg').css({
+		'background': 'url(../img/' + bgname + ') no-repeat center center'
+	});
 
+	// 透明度慢慢恢复为1
+	opacityIncrease();
+	
+	function opacityIncrease() {
+		if (bgopacity < 1) {
+			bgopacity += 0.05;
+			$('#teambg').css({
+				'opacity': bgopacity
+			});
+			setTimeout(opacityIncrease, 50);
+		} else {}
+	}
+	
+	setTimeout(teamBgChange,4500);
+}
 
+setTimeout(teamBgShow,1000);
+setInterval(teamBgShow, 7500);
