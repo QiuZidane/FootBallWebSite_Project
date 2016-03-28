f17631abe2d2046f5989de469e353a5d2f7a62ec
kfzx-qiusd <qiusd@sdc.icbc.com.cn>
2016-03-28 22:34:44 +0800

    完成留言板前端逻辑，待整合

--- src/com/demo/GetBBSData.java
+++ src/com/demo/GetBBSData.java
@@ -1,13 +1,11 @@
 package com.demo;
 
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,16 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class UpLoadBBSData
+ * Servlet implementation class GetBBSData
  */
-@WebServlet("/UpLoadBBSData")
-public class UpLoadBBSData extends HttpServlet {
+@WebServlet("/GetBBSData")
+public class GetBBSData extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public UpLoadBBSData() {
+	public GetBBSData() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
@@ -36,50 +34,49 @@ public class UpLoadBBSData extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setContentType("text/html; charset=utf-8");
-		System.err.println(request.getParameter("data"));
 		
-//		System.err.println(request.getServletContext().getRealPath("/"));
-//		
-//		// 获取当前web应用的绝对路径 
+		// 获取当前web应用的绝对路径 
 //		String absolutePath = request.getServletContext().getRealPath("/");
+////		System.err.println("absolutePath="+absolutePath+"\n");
 //		// 在此路径上面创建文件
 //		File bbsdataFile = new File(absolutePath+"bbsdata.txt");
-//		
-////		System.out.println("bbsdataFile="+bbsdataFile);
-//		
-//		try {
-//			createFile(bbsdataFile);
-//			writeTxtFile(bbsdata, bbsdataFile);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		String retJsonStr;
+		try {
+			retJsonStr = ""
+					+"{"
+					+ "\"1900-04-11:22:11:33\" : { "
+					+"\"imgsrc\": \"http://127.0.0.1:8020/WebSiteSrc/bbs/img/face1.gif\",  "
+					+"\"username\": \"zidane\" , "
+					+"\"msginfo\": \"很好很好啊哈哈哈哈哈！！！！\" , "
+					+"\"smtimes\": \"03月28日 21:22\"  "
+					+ "}"
+					+ ","
+					+ "\"1900-04-11:22:11:43\" :  { "
+					+"\"imgsrc\": \"http://127.0.0.1:8020/WebSiteSrc/bbs/img/face2.gif\",  "
+					+"\"username\": \"even\",  "
+					+"\"msginfo\": \"的路口附近的谁离开房间了大开杀戒哈哈！！！！\",  "
+					+"\"smtimes\": \"03月29日 21:22\"  "
+					+ "}"
+					+ ","
+					+ "\"1900-04-11:22:11:53\" :  { "
+					+"\"imgsrc\": \"http://127.0.0.1:8020/WebSiteSrc/bbs/img/face8.gif\",  "
+					+"\"username\": \"even\",  "
+					+"\"msginfo\": \"的路口附近的似懂非懂是辅导费！！\" , "
+					+"\"smtimes\": \"03月30日 21:22\"  "
+					+ "}"				
+					+ "}"
+					+ "";
+			PrintWriter out = response.getWriter();
+			out.flush();
+			out.write(retJsonStr.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 
+		System.out.println("获取bbs数据");
 	}
-
-
-	/** 
-	 * 创建文件 
-	 * @param fileName 
-	 * @return 
-	 */  
-	public static boolean createFile(File fileName)throws Exception{  
-		boolean flag=false;  
-//		System.out.println("开始创建文件");
-		try{  
-			if(!fileName.exists()){  
-				fileName.createNewFile();  
-				flag=true;  
-//				System.out.println("创建文件成功");
-			}  
-		}catch(Exception e){  
-			e.printStackTrace();  
-//			System.err.println("创建文件失败");
-		}  
-		System.out.println("flag="+flag);
-		return flag;  
-	} 
 
 	/** 
 	 * 读TXT文件内容 
@@ -87,7 +84,7 @@ public class UpLoadBBSData extends HttpServlet {
 	 * @return 
 	 */  
 	public static String readTxtFile(File fileName)throws Exception{  
-		String result=null;  
+		String result="";  
 		FileReader fileReader=null;  
 		BufferedReader bufferedReader=null;  
 		try{  
@@ -114,32 +111,6 @@ public class UpLoadBBSData extends HttpServlet {
 		System.out.println("读取出来的文件内容是："+"\r\n"+result);  
 		return result;  
 	} 
-
-	// 写文件
-	public static boolean writeTxtFile(String content,  File fileName)throws Exception{  
-		RandomAccessFile mm=null;  
-		boolean flag=false;  
-		FileOutputStream o=null;
-		try {  
-			o = new FileOutputStream(fileName, true);		
-			o.write(content.getBytes("UTF-8"));
-			o.close();  
-			//   mm=new RandomAccessFile(fileName,"rw");  
-			//   mm.writeBytes(content);  
-			flag=true;  
-		} catch (Exception e) {  
-			// TODO: handle exception  
-			e.printStackTrace();  
-		}finally{  
-			if(mm!=null){  
-				mm.close();  
-			}  
-		}  
-		return flag;  
-	}  
-
-
-
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
